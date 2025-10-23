b49b30beb362260b23623d4007e1ac65a446941d
Lizardman175 <lizardman175@gmail.com>
2025-10-23 15:27:01 -0600

    Auto build buildings (#24)
    * Added option for AutoBuilding; combined 2 files
    
    * Added Town building map addresses
    
    * Added auto building feature

--- DC1AP/Constants/MiscConstants.cs
+++ DC1AP/Constants/MiscConstants.cs
@@ -1,8 +1,30 @@
-﻿namespace DC1AP.Constants
+﻿
+namespace DC1AP.Constants
 {
+    internal enum Towns
+    {
+        Norune,
+        Matataki,
+        Queens,
+        Muska,
+        Factory,
+        Castle
+    }
+
+    internal enum AutobuildFlags
+    {
+        Off = 0,
+        Any = 1,
+        Hundo = 2,
+        Muska = 3,
+        Robot = 4,
+        MuskaRobot = 5
+    }
+
     internal class MiscConstants
     {
         internal const int BaseId = 971_110_000;
+
         internal const int MaxAtlaPerFloor = 8;
         internal const int GeoMaxItemCount = 120;
 
@@ -10,5 +32,22 @@
         internal const int AtlaUnavailable = -1;
         internal const int AtlaAvailable = -2;
         internal const int AtlaClaimed = -3;
+
+        // All building IDs are in the .json.  Some are needed for edge cases here.
+        internal const short NoruneRiverId = 15;
+        internal const short NoruneBridgeId = 16;
+        internal const short CouscousId = 5;
+        internal const short MushroomId = 7;
+        internal const short EarthAId = 18;
+        internal const short EarthBId = 19;
+        internal const short Watermill1Id = 11;
+        internal const short Watermill2Id = 12;
+        internal const short Watermill3Id = 13;
+        internal const short MatatakiRiverId = 16;
+        internal const short MatatakiBridgeId = 17;
+
+        internal const short MatatakiReqRiverCount = 5;
+        internal const short MatatakiBridgeRiverCount = 6;
+        internal const short NoruneBridgeRiverCount = 3;
     }
 }
---
+++ DC1AP/Georama/BuildingCoords.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DC1AP.Georama
+{
+    public class BuildingCoords
+    {
+        public short Orientation = 0;
+
+        // These are actually floats, but we're just stuffing raw byte values into the memory.
+        public uint X;
+        public float Y;
+        public uint Z;
+    }
+}
